d78eaebc3ad411772d9787ec4616ee07a9608889
Adam Tassara Powell <apowell@umass.edu>
2022-07-24 18:40:45 -0400

    Reorganized resume; began splitting content from form

---
+++ content.py
@@ -0,0 +1,22 @@
+projects = [
+    {
+        "name": "BA-T BASIC",
+        "role": "Developer",
+        "start_time": "December 2017",
+        "end_time": "April 2018",
+        "bullets": [
+            "Built a BASIC Development Environment in Javascript, with a text and font editor, that allows users to create games in the browser.",
+            "Designed a single pass compiler to convert BASIC code into instructions for a stack based virtual machine.",
+        ],
+    },
+    {
+        "name": "Onehourgamejam.com",
+        "role": "Contributor",
+        "start_time": "March 2019",
+        "end_time": "April 2019",
+        "bullets": [
+            "Revamped the theme voting system and administrative functions of an open-source PHP game jam website.",
+            "Independently discovered a vulnerability on the site which allowed attackers to operate administrative functions from malicious webpages and fixed the issue by modifying session code to add a randomized token to forms.",
+        ],
+    },
+]
--- output.py
+++ output.py
@@ -1,6 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
+import content
+
 env = Environment(loader=FileSystemLoader('templates'))
 
-resume = env.get_template("resume.html")
+resume = env.get_template("base.html")
 
-print(resume.render())
+print(resume.render(projects=content.projects))
