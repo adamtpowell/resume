bdce15f96c4c11a2e9d48850f6ee9a6a423a868d
Adam Tassara Powell <apowell@umass.edu>
2022-12-02 15:52:08 -0500

    Cleanup repo for public release

--- content.py
+++ content.py
@@ -1,57 +1,72 @@
 content = {
     "name": "Adam Tassara Powell",
     "contact_info": {
-        "location": "Amherst MA / Arlington, MA",
-        "phone_number": "(781)507-6553",
-        "email": "apowell@umass.edu", # TODO: Create a professional address.
-        "website": "github.com/adamtpowell",
+        "location": "Amherst, MA / Arlington, MA",
+        "phone_number": {
+            "text": "(781)507-6553",
+            "url": "+1-781-507-6553",
+        },
+        "email": "adam@atpowell.com", # TODO: Create a professional address.
+        "website": {
+            "text": "github.com/adamtpowell",
+            "url": "https://github.com/adamtpowell",
+        }
     },
     "education": [
         {
             "name": "University of Massachusetts Amherst",
             "degree": "B.S. in Computer Science",
             "end_time": "May 2022",
-            "gpa": "GPA: TODO",
         },
-        {
-            "name": "Minuteman High School",
-            "degree": "High School with Major in Programming and Web Development",
-            "end_time": "May 2018",
-            "gpa": "Graduated as Valedictorian",
-        },
+        # {
+        #     "name": "Minuteman High School",
+        #     "degree": "High School with Major in Programming and Web Development",
+        #     "end_time": "May 2018",
+        #     "gpa": "Graduated as Valedictorian",
+        # },
     ],
-    "courses": [ # Note: These may be out of date.
-        "Algorithms",
-        "Data Structures",
-        "Intro to Computer Systems (Unix / C)",
-        "Reasoning Under Uncertainty (Statistics)",
-        "Make (Arduino / Electronics)",
-        "Artificial Intelligence",
-        "Human Computer Interaction",
-        "Operating Systems",
-        "Calculus III",
-        "Intro to Software Engineering",
-        "Inside the Box (Architecture / ARM)",
-        "Linear Algebra",
-        "Intro to Computation (Discrete Math)",
-        "Programming Methodology",
-    ],
-    "skills": [
-        "Python",
-        "Javascript",
-        "HTML",
-        "CSS",
-        "Node.js",
-        "Java",
-        "C#",
-        "PHP",
-        "C",
-        "SQL",
-        "Lua",
-        "Linux",
-        "Docker",
-        "Git",
-    ],
+    # "courses": [ # Note: These may be out of date.
+    #     "Algorithms",
+    #     "Data Structures",
+    #     "Intro to Computer Systems (Unix / C)",
+    #     "Reasoning Under Uncertainty (Statistics)",
+    #     "Make (Arduino / Electronics)",
+    #     "Artificial Intelligence",
+    #     "Human Computer Interaction",
+    #     "Operating Systems",
+    #     "Calculus III",
+    #     "Intro to Software Engineering",
+    #     "Inside the Box (Architecture / ARM)",
+    #     "Linear Algebra",
+    #     "Intro to Computation (Discrete Math)",
+    #     "Programming Methodology",
+    # ],
+    "skills": {
+        "Languages": [
+            "Python",
+            "Javascript",
+            "HTML",
+            "CSS",
+            "Java",
+            "SQL",
+            # "C#",
+            "C",
+            "PHP",
+            # "Lua",
+        ],
+        "Frameworks": [
+            "React",
+            "Flask",
+            "Node.js",
+        ],
+        "Tooling / Architecture": [
+            "Linux",
+            "Docker",
+            "Git",
+            "Bash",
+            "REST APIs"
+        ]
+    },
     "internships": [
         {
             "name": "Raptor Maps",
@@ -61,7 +76,8 @@ content = {
             "bullets": [
                 "Worked with experienced developers to create and refine new products for the solar industry.",
                 "Refactored local development environments using Docker and docker-compose.",
-                "Developed a full-stack dashboard using Python, React, and PostgreSQL to support the release of a new product.",
+                # "TODO: Something about the timeseries sensor work; bugfixes related to time and space. What was the graph bug? Check notes."
+                "Developed a full-stack dashboard using Python, React, and Postgres for the release of a new product.",
                 "Worked with other developers to plan future backend patterns to be used throughout the company."
             ],
         },
@@ -71,7 +87,8 @@ content = {
             "start_time": "August 2019",
             "end_time": "September 2019",
             "bullets": [
-                "Independently rewrote the legacy front end of a classic ASP site, decreasing load time by 30% while adding mobile support and modernizing the visual design.",
+                "Independently rewrote the legacy front end of a classic ASP site",
+                "Decreased load time by 30% while adding mobile support and modernizing the visual design.",
             ],
         },
     ],
@@ -80,20 +97,10 @@ content = {
             "name": "Animation Ring",
             "role": "Developer",
             "start_time": "January 2022",
-            "end_time": "TODO",
+            "end_time": "March 2022",
             "bullets": [
-                "Worked with a client to create an application to support an upcoming kickstarter.",
-                "Designed and developed an application to credate and print barrier-grid style animations for creation of custom rings.",
-            ],
-        },
-        {
-            "name": "BA-T BASIC",
-            "role": "Developer",
-            "start_time": "December 2017",
-            "end_time": "April 2018",
-            "bullets": [
-                "Built a BASIC Development Environment in Javascript, with a text and font editor, that allows users to create games in the browser.",
-                "Designed a single pass compiler to convert BASIC code into instructions for a stack based virtual machine.",
+                "Worked with a client to support the launch of a Kickstarter project.",
+                "Designed and developed a React and Three.js application to create and print barrier-grid style animations for fidget rings.", # TODO: Rewrite
             ],
         },
         {
@@ -103,37 +110,45 @@ content = {
             "end_time": "April 2019",
             "bullets": [
                 "Revamped the theme voting system and administrative functions of an open-source PHP game jam website.",
-                "Independently discovered a vulnerability on the site which allowed attackers to operate administrative functions from malicious webpages and fixed the issue by modifying session code to add a randomized token to forms.",
+                "Independently discovered a vulnerability on the site which allowed attackers to operate administrative functions from malicious webpages and fixed the issue by modifying sitewide form generation code.",
+            ],
+        },
+        {
+            "name": "BA-T BASIC",
+            "role": "Developer",
+            "start_time": "December 2017",
+            "end_time": "April 2018",
+            "bullets": [
+                "Built a BASIC Development Environment in Javascript that allows users to create games in the browser, with a custom text editor, sprite editor, and runtime.",
+                "Designed a single pass compiler to convert BASIC code into instructions for a stack based virtual machine.",
             ],
         },
     ],
     "hackathons": [
         {
             "name": "Surchengine.net",
-            "role": "HackUMass 2019 Participant",
+            "role": "HackUMass 2019",
             "start_time": "October 2019",
-            "end_time": "October 2019",
             "bullets": [
                 "Worked for 36 hours with a partner to build a search engine targeting phishing, scam, and malware websites.",
                 "Used Vue.js, Node, SQL, and headless Chrome to index and take screenshots of websites from common blocklists.",
             ],
         },
-        {
-            "name": "Punchcards.py",
-            "role": "HackHolyoke 2018 Participant",
-            "start_time": "November 2018",
-            "end_time": "November 2018",
-            "bullets": [
-                "Collaborated with a partner over 24 hours to write a web-app based scanner to remotely load code from punch cards using Python and OpenCV.",
-            ],
-        },
+        # {
+        #     "name": "Punchcards.py",
+        #     "role": "HackHolyoke 2018",
+        #     "start_time": "November 2018",
+        #     "bullets": [
+        #         "Collaborated with a partner over 24 hours to write a web-app based scanner to remotely load code from punch cards using Python and OpenCV.",
+        #     ],
+        # },
         {
             "name": "Mothmobile",
             "role": "Best Robotics Hack, HackUMass 2018",
             "start_time": "October 2018",
-            "end_time": "October 2018",
             "bullets": [
-                "Worked with a small team over 36 hours to build a wheeled Arduino-based robot which mimicked a moth's response to light, winner Best Robotics Hack at HackUMass 2018.",
+                "Worked with a small team over 36 hours to build a wheeled Arduino-based robot which mimicked a moth's response to light.",
+                "Won Best Robotics Hack at HackUMass 2018.",
             ],
         },
     ],
@@ -156,10 +171,10 @@ content = {
             "start_time": "2017",
             "end_time": "2018",
         },
-        {
-            "name": "Ludum Dare Game Jam Compo",
-            "role": "Seventh place out of over one thousand entries in category \"Overall\".",
-            "start_time": "2018",
-        }
+        # {
+        #     "name": "Ludum Dare Game Jam Compo",
+        #     "role": "Seventh place out of over one thousand entries in category \"Overall\".",
+        #     "start_time": "2018",
+        # }
     ],
 }
